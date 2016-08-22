michelmansour/virgil

commit dd3820cb202a56ec13b8c7173be2aaac5d3ff27c

Make poem list sortable
- Use sortablejs for reordering
- Teach POST api/poem route to save an entire list of poems

server.js
@@ -24,31 +24,35 @@ app.get('/api/poem', function (req, res) {
 });
 
 app.post('/api/poem', function (req, res) {
-  fs.readFile(POEMS_FILE, function (err, data) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    var poems = JSON.parse(data);
-    // NOTE: In a real implementation, we would likely rely on a database or
-    // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
-    // treat Date.now() as unique-enough for our purposes.
-    var newPoem = {
-      id: Date.now(),
-      title: req.body.title,
-      author: req.body.author,
-      collection: req.body.collection,
-      text: req.body.text
-    };
-    poems.push(newPoem);
-    fs.writeFile(POEMS_FILE, JSON.stringify(poems, null, 4), function (err) {
+  if (req.body.poems && Array.isArray(req.body.poems)) {
+    poems = req.body.poems;
+  } else {
+    fs.readFile(POEMS_FILE, function (err, data) {
       if (err) {
         console.error(err);
         process.exit(1);
       }
-      res.json(poems);
+      poems = JSON.parse(data);
+      // NOTE: In a real implementation, we would likely rely on a database or
+      // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
+      // treat Date.now() as unique-enough for our purposes.
+      var newPoem = {
+        id: Date.now(),
+        title: req.body.title,
+        author: req.body.author,
+        collection: req.body.collection,
+        text: req.body.text
+      };
+      poems.push(newPoem);
     });
+  }
+  fs.writeFile(POEMS_FILE, JSON.stringify(poems, null, 4), function (err) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
   });
+  res.json(poems);
 });
 
 app.listen(3000, function () {

client/src/PoemBox.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Sortable from 'sortablejs';
 import $ from 'jquery';
 import PoemList from './PoemList';
 import AddPoemForm from './AddPoemForm';
@@ -34,9 +35,33 @@ var PoemBox = React.createClass({
       }.bind(this)
     });
   },
+  savePoems: function () {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: {poems: this.state.data},
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+          console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
   componentDidMount: function () {
     this.loadPoems();
     setInterval(this.loadPoems, this.props.pollInterval);
+    var el = document.getElementById('mainPoemList');
+    Sortable.create(el, {
+      onUpdate: function (evt) {
+        let reordering = this.state.data.slice();
+        let movedPoem = reordering.splice(evt.oldIndex, 1);
+        reordering.splice(evt.newIndex, 0, ...movedPoem);
+        this.setState({data: reordering});
+        this.savePoems();
+      }.bind(this)
+    });
   },
   render: function () {
     return (

client/src/PoemList.js
@@ -8,7 +8,7 @@ const PoemList = ({ data }) => {
     );
   });
   return (
-    <div className="poemList">
+    <div className="poemList" id='mainPoemList'>
       {poemNodes}
     </div>
   );

commit e3e965c14772489eddda3ca3822399539bedca46

Fix bug in adding a poem
The bug was introduced in dd3820c. The fix is to make sure to wait for
the read and write file operations to finish before sending the
response.

Also make the server startup message a bit more relevant.

server.js
@@ -25,18 +25,22 @@ app.get('/api/poem', function (req, res) {
 
 app.post('/api/poem', function (req, res) {
   if (req.body.poems && Array.isArray(req.body.poems)) {
-    poems = req.body.poems;
+    let poems = req.body.poems;
+    fs.writeFile(POEMS_FILE, JSON.stringify(poems, null, 4), function (err) {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+      res.json(poems);
+    });
   } else {
     fs.readFile(POEMS_FILE, function (err, data) {
       if (err) {
         console.error(err);
         process.exit(1);
       }
-      poems = JSON.parse(data);
-      // NOTE: In a real implementation, we would likely rely on a database or
-      // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
-      // treat Date.now() as unique-enough for our purposes.
-      var newPoem = {
+      let poems = JSON.parse(data);
+      let newPoem = {
         id: Date.now(),
         title: req.body.title,
         author: req.body.author,
@@ -44,17 +48,17 @@ app.post('/api/poem', function (req, res) {
         text: req.body.text
       };
       poems.push(newPoem);
+      fs.writeFile(POEMS_FILE, JSON.stringify(poems, null, 4), function (err) {
+        if (err) {
+          console.error(err);
+          process.exit(1);
+        }
+        res.json(poems);
+      });
     });
   }
-  fs.writeFile(POEMS_FILE, JSON.stringify(poems, null, 4), function (err) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-  });
-  res.json(poems);
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Server started and listening on port 3000!');
 });