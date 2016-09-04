3e7f92f3c8e759f02e945cf5df6e75aa46e394d2
Michel Mansour <michel.mansour@gmail.com>
2016-09-03 23:32:08 -0400

    Add user authentication
    - Use JSON web tokens (JWT) from user authentication
    - Add protection to all server-side routes
    - Add login route on server to handle user login
    - Add file to store registered users
    - Add login, logout, and authenticated request functionality to client
    - Update AJAX requests in client components to request with
      authentication token

---
+++ client/src/auth.js
@@ -0,0 +1,66 @@
+import $ from 'jquery';
+
+function performLogin(username, password, cb) {
+  $.ajax({
+    url: '/login',
+    dataType: 'json',
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify({ username, password }),
+    success: (data) => {
+      cb({ authenticated: true, token: data.token });
+    },
+    error: (xhr, status, err) => {
+      console.error('/login', status, err.toString());
+      cb({ authenticated: false, token: null });
+    },
+  });
+}
+
+function onChange() {}
+
+function login(email, pass, cb) {
+  if (sessionStorage.token) {
+    if (cb) cb(true);
+    onChange(true);
+    return;
+  }
+  performLogin(email, pass, (res) => {
+    if (res.authenticated) {
+      sessionStorage.token = res.token;
+      if (cb) cb(true);
+      onChange(true);
+    } else {
+      if (cb) cb(false);
+      onChange(false);
+    }
+  });
+}
+
+function logout(cb) {
+  delete sessionStorage.token;
+  if (cb) cb();
+  onChange(false);
+}
+
+function loggedIn() {
+  return !!sessionStorage.token;
+}
+
+function requireAuth(nextState, replace) {
+  if (!loggedIn()) {
+    replace({
+      pathname: '/login',
+      state: { nextPathname: nextState.location.pathname },
+    });
+  }
+}
+
+function requestWithAuth(ajaxConfig) {
+  $.ajax({
+    headers: { Authorization: `Bearer ${sessionStorage.token}` },
+    ...ajaxConfig,
+  });
+}
+
+export { login, logout, loggedIn, requireAuth, requestWithAuth };
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+import { requireAuth } from './auth';
 import App from './App';
+import Login from './Login';
+import Logout from './Logout';
 import PoemListContainer from './PoemListContainer';
 import PoemContainer from './PoemContainer';
 
 render((
   <Router history={browserHistory}>
     <Route path="/" component={App}>
-      <IndexRoute component={PoemListContainer} url="/api/poem" pollInterval={2000} />
-      <Route path="/poem/:poemId" component={PoemContainer} />
+      <IndexRoute component={PoemListContainer} url="/api/poem" pollInterval={2000} onEnter={requireAuth} />
+      <Route path="/poem/:poemId" component={PoemContainer} onEnter={requireAuth} />
     </Route>
+    <Route path="/login" component={Login} />
+    <Route path="/logout" component={Logout} onEnter={requireAuth} />
   </Router>
   ), document.getElementById('app')
 );
